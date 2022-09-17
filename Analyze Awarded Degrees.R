5d4fa461a51040dc2cf9d1497553f28993c72f1c
connoryen <connor.yen@berkeley.edu>
2022-09-17 16:49:38 -0700

    removed eroneous file

--- Analyze Awarded Degrees.R
+++
@@ -1,9 +0,0 @@
-source("Process Completions.R")
-
-df <- degrees_awarded(deg_type = "doctorate", full.data = TRUE)
-
-df %>%
-  ggplot(aes(x = year, y = n.students)) + 
-  geom_point() +
-  geom_line() + 
-  theme_minimal()
