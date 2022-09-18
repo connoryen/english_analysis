71be46df7073b09b49bc2b0e3d99297cc82d3c76
connoryen <connor.yen@berkeley.edu>
2022-09-17 18:40:40 -0700

    plots for percentage of national degrees awarded. Plots for ratio between BAs and PhDs

--- Berkeley Trends.R
+++ Berkeley Trends.R
@@ -1,56 +1,106 @@
-source("Process Completions.R")
+df <- read.csv("data/processed_completions.csv")
 
 # ======================================================================
-# Compare number of awarded doctoral and bachelors degrees
+# Ratio between bachelors and doctorate degrees
 # ======================================================================
-df_ber <- degrees_awarded(my_names = c("Berkeley"), 
-                           deg_type = c("doctorate", "bachelors"))
 
-t = df_ber$year[df_ber$deg_type == "bachelors"]
-bac_ber = df_ber$n.students[df_ber$deg_type == "bachelors"]
-doc_ber = df_ber$n.students[df_ber$deg_type == "doctorate"]
+# Begin plot: private inst comparison ----------------------------------
+df_rat <- df %>%
+  dplyr::filter(year %in% c(2000:2020)) %>%
+  dplyr::group_by(id, name, cip, year) %>%
+  dplyr::summarise(ratio = n.students[deg == "bachelors"]/n.students[deg == "doctorate"]) %>%
+  dplyr::mutate(ratio = replace_na(ratio, 0)) %>%
+  dplyr::mutate(ratio = ifelse(is.infinite(ratio), 0, ratio))
 
-# Begin plot -----------------------------------------------------------
-# space for second y-axis - change margins (`mar`) parameter
-par(mar = c(4, 4, 2, 4) + 0.3)
-# Create first plot
-plot(x=t, y=bac_ber,
-     type = "o", lwd = 2, col = "dodgerblue",
-     cex.axis = 0.7,
-     main = "Awarded Degrees Nationally", 
-     xlab = "year", ylab = "bachelors")
-# Add new plot
-par(new = TRUE)
-# Create second plot without axes
-plot(x=t, y=doc_ber,
-     type = "o", lwd = 2, col = "hotpink", 
-     axes = FALSE, xlab = "", ylab = "")
-# add second axis. Note that `las` parameter changes rotation
-axis(side = 4, at = pretty(range(doc_ber)), cex.axis = 0.7, las = 1)
-# Add second axis label
-mtext("doctorate", side = 4, line = 3, las = 3)
-#legend
-legend(x = "bottomleft", legend=c("bachelors", "doctorate"),
-       col=c("dodgerblue", "hotpink"), lty=c(1,1), 
-       pch = c(1,1), lwd = c(2,2), cex = 0.75)
+pat = paste0(c("Berkeley", 
+               "Yale",
+               "Princeton",
+               "Stanford",
+               "Cornell",
+               "University of Chicago"), 
+             collapse = "|")
+df_rat %>%
+  dplyr::filter(grepl(pat, name)) %>%
+  ggplot(aes(x=year, y=ratio, color = name)) +
+  geom_line() + 
+  geom_point() + 
+  labs(title = "BA:PhD Private") + 
+  theme_minimal()
+
+# Begin plot: public inst comparison -----------------------------------
+pat = paste0(c("Berkeley", 
+               "University of California-Los Angeles",
+               "University of California-Irvine",
+               "University of Michigan-Ann Arbor",
+               "University of Illinois Urbana-Champaign",
+               "University of Virginia-Main Campus"), 
+             collapse = "|")
+
+df_rat %>%
+  dplyr::filter(grepl(pat, name)) %>%
+  ggplot(aes(x=year, y=ratio, color = name)) +
+  geom_line() + 
+  geom_point() + 
+  labs(title = "BA:PhD Public") + 
+  theme_minimal()
+
+# ======================================================================
+# Percent of doctorate degrees awarded by Berkeley
+# ======================================================================
+
+#' Plot the percent of total national awarded English degrees contributed 
+#' by a university (or group of universities). 
+#'
+#' @param my_inst a regex pattern of institutions
+#' 
+percent_of_national <- function(df, my_inst) {
+  my_df <- df %>%
+    dplyr::filter(grepl(my_inst, name),
+                  year %in% c(1984:2020)) %>%
+    dplyr::group_by(cip,year,deg) %>%
+    dplyr::summarise(n.students = sum(n.students))
+  
+  df_nat <- df %>%
+    dplyr::filter(year %in% c(1984:2020)) %>%
+    dplyr::group_by(cip, year, deg) %>%
+    dplyr::summarise(n.students = sum(n.students))
+  
+  df_per_nat <- my_df %>%
+    dplyr::inner_join(df_nat, by = c('year'='year', 'deg'='deg', 'cip'='cip')) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(per = n.students.x/n.students.y*100) %>%
+    dplyr::select(year, cip, deg, per)
+  
+  df_per_nat %>%
+    ggplot(aes(x=year, y=per, color=deg)) +
+    geom_line() + 
+    geom_point() + 
+    labs(title = my_inst,
+         y = "Percent of National (%)") + 
+    theme_minimal()
+}
+
+percent_of_national(df, "University of California-Irvine")
 
 # ======================================================================
 # Compare number of degrees awarded at Berkeley to national statistics
 # ======================================================================
+df_ber <- df %>%
+  dplyr::filter(grepl("Berkeley", name),
+                year %in% c(2000:2020)) %>%
+  dplyr::arrange(year, deg)
 
-all_ids <- read.csv("data/NCES_Inst_Details/English PhD granting Universities 2017-18.csv") %>%
-  dplyr::select(UnitID) %>%
-  dplyr::rename(id = UnitID)
+t = df_ber$year[df_ber$deg == "bachelors"]
+bac_ber = df_ber$n.students[df_ber$deg == "bachelors"]
+doc_ber = df_ber$n.students[df_ber$deg == "doctorate"]
 
-# bachelors and doctoral degrees awarded nationally
-# We only consider universities with PhD granting programs. 
-df_nat <- degrees_awarded(my_names = c(), my_ids = all_ids[["id"]],
-                          deg_type = c("doctorate", "bachelors")) %>%
-  dplyr::group_by(year, deg_type) %>%
+df_nat <- df %>%
+  dplyr::filter(year %in% c(2000:2020)) %>%
+  dplyr::group_by(cip, year, deg) %>%
   dplyr::summarise(n.students = sum(n.students))
 
-bac_nat = df_nat$n.students[df_nat$deg_type == "bachelors"]
-doc_nat = df_nat$n.students[df_nat$deg_type == "doctorate"]
+bac_nat = df_nat$n.students[df_nat$deg == "bachelors"]
+doc_nat = df_nat$n.students[df_nat$deg == "doctorate"]
 
 # Begin plot for PhDs ---------------------------------------------------
 # space for second y-axis - change margins (`mar`) parameter
@@ -60,7 +110,7 @@ plot(x=t, y=doc_nat,
      type = "o", lwd = 2, col = "dodgerblue",
      cex.axis = 0.7,
      main = "PhDs: Berkeley vs. National", 
-     xlab = "year", ylab = "bachelors")
+     xlab = "year", ylab = "National")
 # Add new plot
 par(new = TRUE)
 # Create second plot without axes
@@ -70,7 +120,7 @@ plot(x=t, y=doc_ber,
 # add second axis. Note that `las` parameter changes rotation
 axis(side = 4, at = pretty(range(doc_ber)), cex.axis = 0.7, las = 1)
 # Add second axis label
-mtext("doctorate", side = 4, line = 3, las = 3)
+mtext("Berkeley", side = 4, line = 3, las = 3)
 #legend
 legend(x = "bottomleft", legend=c("National", "Berkeley"),
        col=c("dodgerblue", "hotpink"), lty=c(1,1), 
@@ -84,7 +134,7 @@ plot(x=t, y=bac_nat,
      type = "o", lwd = 2, col = "dodgerblue",
      cex.axis = 0.7,
      main = "BAs: Berkeley vs. National", 
-     xlab = "year", ylab = "bachelors")
+     xlab = "year", ylab = "National")
 # Add new plot
 par(new = TRUE)
 # Create second plot without axes
@@ -94,7 +144,7 @@ plot(x=t, y=bac_ber,
 # add second axis. Note that `las` parameter changes rotation
 axis(side = 4, at = pretty(range(bac_ber)), cex.axis = 0.7, las = 1)
 # Add second axis label
-mtext("doctorate", side = 4, line = 3, las = 3)
+mtext("Berkeley", side = 4, line = 3, las = 3)
 #legend
 legend(x = "bottom", legend=c("National", "Berkeley"),
        col=c("dodgerblue", "hotpink"), lty=c(1,1), 
